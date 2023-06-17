98694162817578f3d0a331ce75e56cb33f215722
Artur <arturdecamargos@gmail.com>
2023-06-17 12:24:11 -0300

    Adição de telas e cruds.

--- TrabalhoOO/src/control/Principal.java
+++ TrabalhoOO/src/control/Principal.java
@@ -1,12 +1,24 @@
 package control;
 
 import view.TelaBoasVindas;
+import model.Cliente;
 
 public class Principal {
 	public static void main(String[] args) {
 		
-		TelaBoasVindas telaBoasVindas = new TelaBoasVindas();
-        telaBoasVindas.initialize();
+		Cliente cliente = new Cliente("calvo", 123456);
+		Cliente cliente2 = new Cliente("lucas", 8756215);
+		Cliente cliente3= new Cliente("pedro", 36874);
+		
+		ControleCliente cc = new ControleCliente();
+		System.out.println(cc.salvarCliente(cliente));
+		System.out.println(cc.salvarCliente(cliente2));
+		System.out.println(cc.salvarCliente(cliente3));
+		
+		
+		
+//		TelaBoasVindas telaBoasVindas = new TelaBoasVindas();
+//        telaBoasVindas.initialize();
 		
 //		TelaPrincipal telaPrincipal = new TelaPrincipal();
 //        telaPrincipal.initialize();
